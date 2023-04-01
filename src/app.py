665cc8a0e1c1218dcf5965c8991af1f6c519890b
Dennis Dunne <dunned024@gmail.com>
2023-04-01 08:49:05 -0400

    create components, include index page

--- src/app.py
+++ src/app.py
@@ -6,30 +6,6 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def main():
     return render_template("index.html")
-    return """
-     <form action="/get_location" method="POST">
-         <input name="location_input">
-         <input type="submit" value="Enter location">
-     </form>
-     """
-
-
-@app.route("/scripts")
-def scripts():
-    return """
-    <script>
-        $SCRIPT_ROOT = {{ request.script_root|tojson }};
-    </script>
-    <script src="{{ url_for('static', filename='jquery.js') }}"></script>
-    <script src="http://maps.googleapis.com/maps/api/js?libraries=places"></script>
-    <script src="jquery.geocomplete.js"></script>
-    """
-
-
-@app.route("/homepage/")
-def interactive():
-    template = url_for("static", filename="homepage.html")
-    return render_template(template)
 
 
 @app.route("/get_location", methods=["POST"])
